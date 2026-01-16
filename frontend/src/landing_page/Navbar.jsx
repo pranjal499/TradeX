ee627e6fafc86e0a2233f3c4341a586adbab3543
pranjal499 <pranjalprofsocial72@gmail.com>
2026-01-16 19:37:37 +0530

    react router setup

--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -1,13 +1,14 @@
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
         // Navbar 
         <nav className="navbar navbar-expand-lg border-bottom sticky-top">
             <div className="container">
-                <a className="navbar-brand" href="#">
+                <Link className="navbar-brand" to='/'>
                     <img src="media/images/logo.png" alt="TradeX Logo" className="logo" />
-                </a>
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -15,19 +16,19 @@ export default function Navbar() {
                     <form className="d-flex" role="search">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link " aria-current="page" href="#">Signup</a>
+                                <Link className="nav-link " aria-current="page" to='/signup'>Signup</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link " aria-current="page" href="#">About</a>
+                                <Link className="nav-link " aria-current="page" to='/about'>About</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link " aria-current="page" href="#">Products</a>
+                                <Link className="nav-link " aria-current="page" to='/products'>Products</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link " aria-current="page" href="#">Pricing</a>
+                                <Link className="nav-link " aria-current="page" to='/pricing'>Pricing</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link " aria-current="page" href="#">Support</a>
+                                <Link className="nav-link " aria-current="page" to='/support'>Support</Link>
                             </li>
                             
                         </ul>
--- frontend/src/landing_page/home/HomePage.jsx
+++ frontend/src/landing_page/home/HomePage.jsx
@@ -10,14 +10,12 @@ import Footer from "../Footer";
 export default function HomePage() {
     return( 
         <>
-            <Navbar />
             <Hero />
             <Awards />
             <Stats />
             <Pricing />
             <Education />
             <OpenAccount />
-            <Footer />
         </>
     )
 }
