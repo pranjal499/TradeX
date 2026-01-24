321523959b77f799fdf2c49ddc1e0bd480c0cf9c
pranjal499 <pranjalprofsocial72@gmail.com>
2026-01-24 12:52:09 +0530

    product page

---
+++ frontend/src/landing_page/products/Hero.jsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+    return (
+        <div className="container">
+            <div className="row text-center my-5 p-3 border-bottom pb-5">
+                <h1 className="fs-2">TradeX Products</h1>
+                <h3 className="text-muted fs-5 mt-2">Sleek, modern, and intuitive trading platforms</h3>
+                <p className="mt-3">Check out our  <a href="" >investment offerings <i class="fa-solid fa-arrow-right-long"></i></a></p>
+            </div>
+        </div>
+    )
+}
