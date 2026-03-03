1ebebd04df7f63272b60dde45b2fd19f8b369cf0
pranjal499 <pranjalprofsocial72@gmail.com>
2026-03-03 22:55:52 +0530

    fetching data

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,30 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const HoldingsModel = require('./model/HoldingsModel');
+const PositionsModel = require('./model/PositionsModel');
+const bodyParser = require('body-parser');
+const cors = require('cors')
 
 // Initialize values:
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 const uri = process.env.MONGO_URL;
+
+// cors and body parser:
+app.use(cors());
+app.use(bodyParser.json());
+
+// All holdings:
+app.get('/allHoldings', async (req, res) => {
+    let allHoldings = await HoldingsModel.find({});
+    res.json(allHoldings);
+});
+
+// All positions:
+app.get('/allPositions', async(req, res) => {
+    let allPositions = await PositionsModel.find({});
+    res.json(allPositions);
+});
 
 // Starting server:
 app.listen(PORT, () => {
