83b0e9bb3eeaed2929b70ae89778e71f57e32708
pranjal499 <pranjalprofsocial72@gmail.com>
2026-03-18 13:19:16 +0530

    css changes

--- frontend/src/landing_page/support/Hero.jsx
+++ frontend/src/landing_page/support/Hero.jsx
@@ -1,27 +1,28 @@
 import './Hero.css';
 
 export default function Hero() {
-    return(
+    return (
         <section className="container-fluid px-5" id="supportHero">
-            <div className="pt-5 mx-5" id='supportWrapper'>
+            <div className="pt-5 mx-5 color-white" id='supportWrapper'>
                 <h4 className='fs-5'>Support portal</h4>
                 <a href="">Track tickets</a>
             </div>
             <div className="row p-3">
-                <div className="col p-3 mx-3">
+                <div className="col p-3 mx-3 color-white">
                     <h1 className='fs-3'>Search for an answer or browse help topics to create a ticket</h1>
-                    <input type="text" placeholder="Eg. how do I activate F&0" className='my-3'/>
+                    <input type="text" placeholder="Eg. how do I activate F&0" className='my-3' />
                     <br />
                     <a href="">Track account opening</a>
                     <a href="">Track segment activation </a>
-                    <a href="">Intraday margins</a> 
+                    <a href="">Intraday margins</a>
                     <a href="">Kite user manual</a>
                 </div>
-                <div className="col p-3 mx-3">
+                <div className="col p-3 mx-3 color-white">
                     <h1 className='fs-3'>Featured</h1>
-                    <ol>
-                        <li><a href="">Current Takeovers and Delisting - January 2024</a></li>
-                        <li><a href="">Latest Intraday leverages - MIS & CO</a></li>
+                    <ol className='color-white'>
+                        <a href="">1. Current Takeovers and Delisting - January 2024</a>
+                        <br />
+                        <a href="">2. Latest Intraday leverages - MIS & CO</a>
                     </ol>
                 </div>
             </div>
